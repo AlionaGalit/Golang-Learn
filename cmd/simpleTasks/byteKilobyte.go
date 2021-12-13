cfa36afdf5f094e04b699d53527b5fda56c59b98
Aliona Galit <alionagalit@gmail.com>
2021-12-14 00:35:56 +0200

    transfer number byte kilobyte

--- cmd/simpleTasks/byteKilobyte.go
+++ cmd/simpleTasks/byteKilobyte.go
@@ -1,1 +1,19 @@
+//Перевести байты в килобайты или наоборот
 package main
+
+import "fmt"
+
+func main() {
+	var num float32
+	var size string
+	fmt.Println("Enter number: ")
+	fmt.Scanln(&num)
+	fmt.Println("Choose change b - byte, or k - kilobyte")
+	fmt.Scanln(&size)
+	switch size {
+	case "b":
+		fmt.Println(num * 1024)
+	case "k":
+		fmt.Printf("%.4f\n", num/1024)
+	}
+}
