42ee7975407105167eb51d8e6d42df4116f2b959
Aliona Galit <alionagalit@gmail.com>
2021-12-14 13:10:29 +0200

    change printf

--- cmd/simpleTasks/step.go
+++ cmd/simpleTasks/step.go
@@ -1,1 +1,20 @@
+//Вывести ряд чисел в диапазоне с шагом
 package main
+
+import "fmt"
+
+func main() {
+	var minNum, maxNum, stepNum int
+	fmt.Println("Enter min number: ")
+	fmt.Scanln(&minNum)
+	fmt.Println("Enter max number: ")
+	fmt.Scanln(&maxNum)
+	fmt.Println("Enter step number: ")
+	fmt.Scanln(&stepNum)
+
+	for i := minNum; i <= maxNum; i += stepNum {
+
+		fmt.Printf("%v ", i)
+	}
+
+}
