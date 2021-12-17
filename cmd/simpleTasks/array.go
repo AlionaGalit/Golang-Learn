40e8e0633c290e20d32e33d74aff2738dc3823a7
Aliona Galit <alionagalit@gmail.com>
2021-12-17 15:49:34 +0200

    array.go

---
+++ cmd/simpleTasks/array.go
@@ -0,0 +1,34 @@
+//Заполнение и вывод массивов
+//https://gospodaretsva.com/filling.html
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	arr1 := [10]int{}
+	rand.Seed(time.Now().UnixNano()) //vvodim sliceainim pereborom
+	for i := range arr1 {
+		arr1[i] = rand.Intn(100)
+	}
+	fmt.Printf("%3d \n", arr1)
+
+	arr2 := [10]int{}
+	var num int
+	for i := range arr2 { //vvodim s kliviaturi
+		fmt.Print("Enter number: ")
+		fmt.Scan(&num)
+		arr2[i] = num
+	}
+	fmt.Printf("%3d \n", arr2)
+
+	arr3 := [10]int{}
+	for i := 0; i < len(arr3); i++ { //summa 1 i 2 massiva zapisivaetsea v tretii
+		arr3[i] = arr1[i] + arr2[i]
+	}
+	fmt.Printf("%3d \n", arr3)
+
+}
