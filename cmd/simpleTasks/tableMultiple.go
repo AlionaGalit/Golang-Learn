8524eb18c9a9f7f6c1f0f20705ed0c4bbf29ccd2
Aliona Galit <alionagalit@gmail.com>
2021-12-17 12:50:14 +0200

    table multiplication change with function

--- cmd/simpleTasks/tableMultiple.go
+++ cmd/simpleTasks/tableMultiple.go
@@ -3,13 +3,25 @@ package main
 
 import "fmt"
 
-func main() {
-	for i := 1; i <= 9; i++ {
+func multipl(num int) {
+	for i := 1; i <= num; i++ {
 		fmt.Println() //perehod na novuiu stroku posle pervogo reada
-		for j := 1; j <= 9; j++ {
+		for j := 1; j <= num; j++ {
 			//	multi := i * j
 			fmt.Printf("%3d  ", i*j) //multi
 
 		}
 	}
+}
+func main() {
+
+	multipl(9)
+	fmt.Println()
+	fmt.Println("---------------------------------------------------------------------------------------------------")
+
+	multipl(20)
+	fmt.Println()
+	fmt.Println("---------------------------------------------------------------------------------------------------")
+
+	multipl(5)
 }
