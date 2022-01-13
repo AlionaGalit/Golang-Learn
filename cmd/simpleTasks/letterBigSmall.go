6935ed7700de057bfcd57eb772db780aa5e0e851
Aliona Galit <alionagalit@gmail.com>
2022-01-13 16:52:43 +0200

    Big and small Letters

--- cmd/simpleTasks/letterBigSmall.go
+++ cmd/simpleTasks/letterBigSmall.go
@@ -1,1 +1,22 @@
+//Количество строчных и прописных букв в строке
 package main
+
+import "fmt"
+
+func main() {
+	var line string
+	fmt.Println("Enter line: ")
+	fmt.Scanln(&line)
+
+	big := 0
+	small := 0
+	for i := range line {
+		if line[i] >= 'a' && line[i] <= 'z' {
+			small += 1
+		} else if line[i] >= 'A' && line[i] <= 'Z' {
+			big += 1
+		}
+	}
+	fmt.Println("big: ", big)
+	fmt.Println("small: ", small)
+}
